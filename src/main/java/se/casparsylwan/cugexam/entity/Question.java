dd0b5d4178047fa31dd222ca3f52794b8ba52111
casparsylwan <casparsylwan@gmail.com>
2022-02-15 00:21:19 +0100

    long text questions

--- src/main/java/se/casparsylwan/cugexam/entity/Question.java
+++ src/main/java/se/casparsylwan/cugexam/entity/Question.java
@@ -16,12 +16,17 @@ public class Question {
     @Id
     private Long questionId;
     private Long questionNumber;
+    @Column(columnDefinition="LONGTEXT")
     private String question;
     private String lang;
 
+    @Column(columnDefinition="LONGTEXT")
     private String response1;
+    @Column(columnDefinition="LONGTEXT")
     private String response2;
+    @Column(columnDefinition="LONGTEXT")
     private String response3;
+    @Column(columnDefinition="LONGTEXT")
     private String correctResponse;
 
     @ManyToOne
